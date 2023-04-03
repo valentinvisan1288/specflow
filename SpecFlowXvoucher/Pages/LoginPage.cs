72b781b4b3f018e98b3c9e76edc6613bbb8f51c7
Valentin Visan <valentinvisan1288@gmail.com>
2023-04-03 20:20:10 +0200

    Folder Structure, Cleanup

--- SpecFlowXvoucher/Pages/LoginPage.cs
+++ SpecFlowXvoucher/Pages/LoginPage.cs
@@ -1,8 +1,32 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
 
-public class Login
+namespace SpecFlowXvoucher.Pages
 {
-	public Login()
-	{
-	}
+    public class LoginPage
+    {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.Id, Using = "username")]
+        private IWebElement usernameInput;
+
+        [FindsBy(How = How.Id, Using = "password")]
+        private IWebElement passwordInput;
+
+        [FindsBy(How = How.CssSelector, Using = "button[type='submit']")]
+        private IWebElement loginButton;
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void Login(string username, string password)
+        {
+            usernameInput.SendKeys(username);
+            passwordInput.SendKeys(password);
+            loginButton.Click();
+        }
+    }
 }
