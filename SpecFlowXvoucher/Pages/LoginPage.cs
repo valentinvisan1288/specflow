f08e720237bd73d89db0cd6fa9fcb8c0369856e5
Valentin <80831210+valentinvisan1288@users.noreply.github.com>
2023-04-03 20:21:02 +0200

    Sf 1 (#1)
    * Page Object Model Template
    
    * Folder Structure, Cleanup

---
+++ SpecFlowXvoucher/Pages/LoginPage.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace SpecFlowXvoucher.Pages
+{
+    public class LoginPage
+    {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.Id, Using = "username")]
+        private IWebElement usernameInput;
+
+        [FindsBy(How = How.Id, Using = "password")]
+        private IWebElement passwordInput;
+
+        [FindsBy(How = How.CssSelector, Using = "button[type='submit']")]
+        private IWebElement loginButton;
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void Login(string username, string password)
+        {
+            usernameInput.SendKeys(username);
+            passwordInput.SendKeys(password);
+            loginButton.Click();
+        }
+    }
+}
